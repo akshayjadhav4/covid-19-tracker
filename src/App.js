a772a5b5ecd7e31ee07b35503f81851fd2ff80b3
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-22 16:29:26 +0530

    fetching country names in dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React,{useState ,useEffect} from 'react';
 
 import './App.css';
-
+import { FormControl,Select ,MenuItem  } from '@material-ui/core';
 function App() {
+  
+  const [countries, setCountries] = useState([
+    'INDIA','USA','UK'
+  ])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries)
+        });
+    };
+
+    getCountriesData()
+  }, [])
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="app_header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value=" ">
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
