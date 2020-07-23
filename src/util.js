4666cf553f5ea7e9f406183856a2894ff274cbf5
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-23 16:52:10 +0530

    rendering circles on map

--- src/util.js
+++ src/util.js
@@ -1,3 +1,22 @@
+import React from 'react'
+import { Circle , Popup } from "react-leaflet";
+import numeral from "numeral";
+
+const casesTypesColors = {
+  cases: {
+    hex: "#CC1034",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 2000,
+  },
+};
+
 export const sortData = (data) => {
   const sortedData = [...data];
   sortedData.sort((a, b) => {
@@ -28,3 +47,31 @@ export const buildChartData = (data, casesType = "cases") => {
   };
   return chartData;
 };
+
+
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypesColors[casesType].hex}
+      fillColor={casesTypesColors[casesType].hex}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypesColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div>
+          <div className="info_flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info_name">{country.country}</div>
+          <div className="info_confirmed">Cases : {numeral(country.cases).format("0,0")}</div>
+          <div className="info_recovered">Recovered : {numeral(country.recovered).format("0,0")}</div>
+          <div className="info_deaths">Deaths : {numeral(country.deaths).format("0,0")}</div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
+
+
