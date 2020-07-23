3747f0c5bd0d64dd57d50977e9e7c0922b9c4883
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-23 18:03:25 +0530

    styling according to active status

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "../InfoBox.css"
-function InfoBox({ title, cases, total }) {
+function InfoBox({ isRed,active,title, cases, total,...props }) {
   return (
-    <Card className="infoBox">
+    <Card className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`} onClick={props.onClick}>
       <CardContent>
         <Typography color="textSecondary" className="infoBox_title">
           {title}
         </Typography>
-        <h2 className="infoBox_cases">{cases}</h2>
+        <h2 className={`infoBox_cases  ${!isRed && "infoBox_cases--green"}`}>{cases}</h2>
         <Typography color="textSecondary" className="infoBox_total">
           {total} Total
         </Typography>
