5af0f81d51d93d828f41b45ab4b18d5e5bcfd7ac
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-23 13:43:57 +0530

    Line Graph module

---
+++ src/components/LineGraph.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { buildChartData } from "../util";
+import numeral from "numeral";
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRation: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: true,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+function LineGraph({ casesType = "cases" }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+      .then((response) => response.json())
+      .then((data) => {
+        const chartData = buildChartData(data);
+        setData(chartData);
+      });
+
+  }, [casesType]);
+  return (
+    <div className="line_grapg">
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: "#FF4848",
+                borderColor: "#E71C23",
+                data: data,
+              },
+            ],
+          }}
+          options={options}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
