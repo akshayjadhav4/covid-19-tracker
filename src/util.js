a1bf734067a34d663f9c1973bdf63eaf4160ea08
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-23 13:43:30 +0530

    preparing data for Line graph

--- src/util.js
+++ src/util.js
@@ -10,3 +10,21 @@ export const sortData = (data) => {
 
   return sortedData
 };
+
+
+export const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+
+  for(let date in data[casesType]) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint, // difference between two dates e.g  cases: 3/25/20: 476376 3/26/20: 539336 3/27/20: 603736
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  };
+  return chartData;
+};
